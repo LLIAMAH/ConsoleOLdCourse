510198045d672d6f5f9e097e9910afce6bf4c1a7
Vasily Himchenko <White_Scorpion@inbox.lv>
2023-03-29 23:01:42 +0300

    Last work.

--- ConsoleAppCore/Classes/ExperimentalClasses.cs
+++ ConsoleAppCore/Classes/ExperimentalClasses.cs
@@ -1,4 +1,7 @@
-﻿namespace ConsoleAppCore.Classes
+﻿using System.Drawing;
+using System.Runtime.CompilerServices;
+
+namespace ConsoleAppCore.Classes
 {
     public struct ValueData
     {
@@ -60,28 +63,71 @@
 
     public class Person
     {
-        private readonly string name;
-        private string surname;
-        private Gender gender;
-        private int a;
+        protected readonly string name;
+        protected string surname;
+        protected Gender gender;
+        public int age;
+
+        public string Surname { get; set; }
+
+        public string Surname1 { get; init; }
+
+        protected string Name
+        {
+            get
+            {
+                Surname = "safasdfasdfasdf";
+                return name;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value < 18)
+                    throw new Exception("Вы слишкорм юны для посещения сайта");
+
+                age = value;
+                //OnUpdate();
+            }
+        }
+
+        public void OutString()
+        {
+            this.Surname = "sdfasdfasdf";
+            var a = this.Surname;
+        }
+
+        public void OutString1(string output)
+        {
+            this.Surname = "sdadfasdfasdf";
+            ;
+            output = "sdadfasdfasdf";
+        }
+
 
         public Person() : this("", ""){}
 
         public Person(string name, string surname) : this(name, surname, Gender.Unknown)
         {
-
+            this.Surname1 = "sdfasdfasdfaswdf";
         }
 
         public Person(string name, string surname, Gender gender)
         {
+            this.Surname1 = "sdfasdfasdfaswdf";
             this.name = name;
-            this.surname = surname;
             this.gender = gender;
         }
 
         public void Test()
         {
-            //this.name = "sdafgsdfgsdfg;sdfl";
+            this.Age = 40;
         }
 
         public void Deconstruct(out string name, out string surname)
@@ -95,7 +141,6 @@
     {
         public void Test()
         {
-            //this.name = "sdafgsdfgsdfg;sdfl";
         }
     }
 
@@ -109,6 +154,96 @@
 
         // public / protected / private
         // internal / private protected /protected internal / file
+
+    }
+
+
+    public enum FigureType
+    {
+
+    }
+
+    public abstract class Figure
+    {
+        protected readonly Point _point;
+        protected readonly Size _size;
+
+        public Figure(Point point, Size size)
+        {
+            _point = point;
+            _size = size;
+        }
+
+        public abstract void Draw();
+    }
+
+    public sealed class Triangle : Figure
+    {
+        public Triangle(Point point, Size size) : base(point, size) { }
+
+        public override void Draw()
+        {
+            Console.WriteLine("Мы нарисовали треугольник!");
+        }
+
+        /*
+        public static void DrawUltraMegaTriangle(this Triangle triangle)
+        {
+            Console.WriteLine("Мы нарисовали супер мега ультра треугольник!");
+        }
+         *
+         */
+    }
+
+    public static class TriangleExtension
+    {
+        public static void DrawUltraMegaTriangle(this Triangle triangle, int a = 5)
+        {
+            if(a > 0)
+                Console.WriteLine("Мы нарисовали супер мега ультра треугольник!");
+            else 
+                triangle.Draw();
+        }
+
+        public static void DrawUltraMegaSquare(this Square triangle)
+        {
+            Console.WriteLine("Мы нарисовали супер мега ультра квадрат!");
+        }
+    }
+
+
+    public class Square : Figure
+    {
+        public Square(Point point, Size size) : base(point, size) { }
+
+        public override void Draw()
+        {
+            Console.WriteLine("Мы нарисовали квадрат!");
+        }
+    }
+
+    public class Circle : Figure
+    {
+        public static int data = 5;
+
+        public Circle(Point point, Size size) : base(point, size) { }
+
+        public override void Draw()
+        {
+            Console.WriteLine("Мы нарисовали круг!");
+        }
+
+        public void Test(int someParameter)
+        {
+            Console.WriteLine($"Some parameter: {someParameter}");
+            Console.WriteLine($"Data: {data}");
+            TestData();
+        }
+
+        public static void TestData()
+        {
+            
+        }
 
     }
 }
--- ConsoleAppCore/Program.cs
+++ ConsoleAppCore/Program.cs
@@ -1,4 +1,5 @@
-﻿using ConsoleAppCore.Classes;
+﻿using System.Drawing;
+using ConsoleAppCore.Classes;
 
 ValueData valueData = new ValueData();
 ClassData classData = new ClassData();
@@ -45,8 +46,65 @@ finally
     
 }
 
+string a = "sdfassfdsdsfd";
+var length = a.Length;
+
 var agedPerson = new PersonAged();
 // agedPerson.a = 6;
 
 
-int a = default;
+var personOld = new PersonOld();
+
+
+//var person1 = new Person();
+//person1.OutString(out person1.surname);
+//person1.OutString(out person1.Surname);
+//person1.OutString1(person1.Surname);
+
+//var string1 = "sadfasdfasdf";
+//string1.Length;
+
+
+var arrayFigures = new Figure[10];
+arrayFigures[0] = new Circle(new Point(), new Size());
+arrayFigures[1] = new Triangle(new Point(), new Size());
+arrayFigures[2] = new Square(new Point(), new Size());  
+arrayFigures[3] = new Circle(new Point(), new Size());
+arrayFigures[4] = new Square(new Point(), new Size());
+arrayFigures[5] = new Triangle(new Point(), new Size());
+arrayFigures[6] = new Circle(new Point(), new Size());
+arrayFigures[7] = new Circle(new Point(), new Size());
+arrayFigures[8] = new Triangle(new Point(), new Size());
+arrayFigures[9] = new Square(new Point(), new Size());
+
+Figure figureSingle = new Circle(new Point(), new Size());
+
+
+for (int i = 0; i < arrayFigures.Length; i++)
+{
+    arrayFigures[i].Draw();
+}
+
+
+((Triangle) arrayFigures[1]).DrawUltraMegaTriangle(7);
+((Triangle)arrayFigures[1]).DrawUltraMegaTriangle(-4);
+
+var staticFig1 = new Circle(new Point(), new Size());
+staticFig1.Test(5);
+Circle.data = 10;
+staticFig1.Test(5);
+var staticFig2 = new Circle(new Point(), new Size());
+staticFig1.Test(5);
+staticFig2.Test(5);
+var staticFig3 = new Circle(new Point(), new Size());
+staticFig1.Test(5);
+staticFig2.Test(5);
+staticFig3.Test(5);
+Circle.data = 7;
+staticFig1.Test(5);
+staticFig2.Test(5);
+staticFig3.Test(5);
+
+
+var strin1g = String.Format("Param1: {0}; Param2: {1}, Param3: {2}", 4, 5, 6);
+

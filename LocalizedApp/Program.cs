6b586e38cdecf284f0e4aa4cc5bc72b3a9f87c1e
Vasily Himchenko <White_Scorpion@inbox.lv>
2023-05-06 20:04:35 +0300

    Localization lection code added

--- LocalizedApp/Program.cs
+++ LocalizedApp/Program.cs
@@ -1,3 +1,6 @@
+using System.Configuration;
+using System.Globalization;
+
 namespace LocalizedApp
 {
     internal static class Program
@@ -10,6 +13,12 @@ namespace LocalizedApp
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+
+            var appConfigValue = ConfigurationManager.AppSettings["DefaultCulture"];
+            CultureInfo culture = new CultureInfo(appConfigValue);
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+
             ApplicationConfiguration.Initialize();
             Application.Run(new FMain());
         }

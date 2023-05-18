20a76298dbc2e1925626a5447fc6ab625f669274
Vasily Himchenko <White_Scorpion@inbox.lv>
2023-05-18 21:36:50 +0300

    DbRecordMoverd to separate file.

--- DatabaseExperimentsNET/FMain.cs
+++ DatabaseExperimentsNET/FMain.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DatabaseExperimentsNET.Model;
 
 namespace DatabaseExperimentsNET
 {
@@ -112,27 +113,6 @@ namespace DatabaseExperimentsNET
                     connection.Close();
                 }
             }
-        }
-    }
-
-    public class DbRecord
-    {
-        public long Id { get; set; }
-        public string Login { get; set; }
-        public string FirstName { get; set; }
-        public string MiddleName { get; set; }
-        public string LastName { get; set; }
-        public DateTime BirthDate { get; set; }
-        public string Country { get; set; }
-        public string City { get; set; }
-        public string Street { get; set; }
-        public string HouseNr { get; set; }
-        public string ApartmentsNr { get; set; }
-
-        public override string ToString()
-        {
-            return
-                $"{Id}, {Login}, {FirstName}, {MiddleName}, {LastName}, {BirthDate}, {Country}, {City}, {Street}, {HouseNr}, {ApartmentsNr}";
         }
     }
 }

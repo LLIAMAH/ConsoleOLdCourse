8ae6050ce93dfa553636d2ae327383ac6efdd7d1
Vasily Himchenko <White_Scorpion@inbox.lv>
2023-05-20 21:02:04 +0300

    Added database operations.

--- DatabaseExperimentsNET/CourseDBDataSet.Designer.cs
+++ DatabaseExperimentsNET/CourseDBDataSet.Designer.cs
@@ -3052,11 +3052,16 @@ SELECT Id, Street, CityId, HauseNr, ApartmentsNr, PersonId FROM Addresses WHERE 
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "17.0.0.0")]
         private void InitCommandCollection() {
-            this._commandCollection = new global::System.Data.SqlClient.SqlCommand[1];
+            this._commandCollection = new global::System.Data.SqlClient.SqlCommand[2];
             this._commandCollection[0] = new global::System.Data.SqlClient.SqlCommand();
             this._commandCollection[0].Connection = this.Connection;
             this._commandCollection[0].CommandText = "SELECT Id, Name, CountryId FROM dbo.CountryCities";
             this._commandCollection[0].CommandType = global::System.Data.CommandType.Text;
+            this._commandCollection[1] = new global::System.Data.SqlClient.SqlCommand();
+            this._commandCollection[1].Connection = this.Connection;
+            this._commandCollection[1].CommandText = "SELECT Id, Name, CountryId\r\nFROM   CountryCities\r\nWHERE (CountryId = @countryId)";
+            this._commandCollection[1].CommandType = global::System.Data.CommandType.Text;
+            this._commandCollection[1].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@countryId", global::System.Data.SqlDbType.BigInt, 8, global::System.Data.ParameterDirection.Input, 0, 0, "CountryId", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
         }
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
@@ -3081,6 +3086,20 @@ SELECT Id, Street, CityId, HauseNr, ApartmentsNr, PersonId FROM Addresses WHERE 
             CourseDBDataSet.CountryCitiesDataTable dataTable = new CourseDBDataSet.CountryCitiesDataTable();
             this.Adapter.Fill(dataTable);
             return dataTable;
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "17.0.0.0")]
+        [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
+        [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Fill, false)]
+        public virtual int FillBy(CourseDBDataSet.CountryCitiesDataTable dataTable, long countryId) {
+            this.Adapter.SelectCommand = this.CommandCollection[1];
+            this.Adapter.SelectCommand.Parameters[0].Value = ((long)(countryId));
+            if ((this.ClearBeforeFill == true)) {
+                dataTable.Clear();
+            }
+            int returnValue = this.Adapter.Fill(dataTable);
+            return returnValue;
         }
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
--- DatabaseExperimentsNET/FMain.cs
+++ DatabaseExperimentsNET/FMain.cs
@@ -31,14 +31,19 @@ namespace DatabaseExperimentsNET
 
         private void FMain_Load(object sender, EventArgs e)
         {
+            // TODO: This line of code loads data into the 'courseDBDataSet.CountryCities' table. You can move, or remove it, as needed.
+            this.countryCitiesTableAdapter.Fill(this.courseDBDataSet.CountryCities);
+            // TODO: This line of code loads data into the 'courseDBDataSet.Countries' table. You can move, or remove it, as needed.
+            this.countriesTableAdapter.Fill(this.courseDBDataSet.Countries);
+
+            UpdateCitiesDataSource();
             this._connectionString = ConfigurationManager
                 .ConnectionStrings["DefaultConnection"].ConnectionString;
-
             this._list = new BindingList<DbRecord>();
-            lbOutput.DataSource = this._list;
+            dgvOutput.DataSource = this._list;
         }
 
-        private void bnLoadData_Click(object sender, EventArgs e)
+        private void UpdateDataView()
         {
             this._list.Clear();
             using (var connection = new SqlConnection(this._connectionString))
@@ -77,7 +82,7 @@ namespace DatabaseExperimentsNET
                                 {
                                     //Convert.ToInt64(reader[0])
                                     //Convert.ToInt64(reader["Id"]),
-                                    Id = Convert.ToInt64(reader[(int)EnumDbColumn.Id]), 
+                                    Id = Convert.ToInt64(reader[(int)EnumDbColumn.Id]),
                                     Login = reader["Login"].ToString(),
                                     FirstName = reader["FirstName"] == DBNull.Value
                                         ? string.Empty
@@ -113,6 +118,177 @@ namespace DatabaseExperimentsNET
                     connection.Close();
                 }
             }
+        }
+
+        private void bnLoadData_Click(object sender, EventArgs e)
+        {
+            UpdateDataView();
+        }
+
+        private void cbCountry_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateCitiesDataSource();
+        }
+
+        private void UpdateCitiesDataSource()
+        {
+            var selectedCountryId = Convert.ToInt64(cbCountry.SelectedValue);
+            try
+            {
+                this.countryCitiesTableAdapter.FillBy(this.courseDBDataSet.CountryCities, selectedCountryId);
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void bnAdd_Click(object sender, EventArgs e)
+        {
+            var newRecord = new InputRecord
+            {
+                Login = tbLogin.Text,
+                FirstName = tbFirstName.Text,
+                MiddleName = tbMiddleName.Text,
+                LastName = tbLastName.Text,
+                BirthDate = dtpBirthDate.Value.Date,
+                CountryId = Convert.ToInt64(cbCountry.SelectedValue),
+                CityId = Convert.ToInt64(cbCity.SelectedValue),
+                Street = tbStreet.Text,
+                HouseNr = tbHouseNr.Text,
+                ApartmentsNr = tbApartmentsNr.Text
+            };
+
+            if (!newRecord.IsValid())
+            {
+                MessageBox.Show("Data for new record are invalid - recheck and refill.");
+                return;
+            }
+
+            using (var connection = new SqlConnection(this._connectionString))
+            {
+                connection.Open();
+                var transaction = connection.BeginTransaction();
+
+                try
+                {
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.Transaction = transaction;
+
+                        command.Parameters.Clear();
+                        command.CommandText = $@"
+                                INSERT INTO [dbo].[Persons]
+                                       ([Login]
+                                       ,[FirstName]
+                                       ,[MiddleName]
+                                       ,[LastName]
+                                       ,[BirthDate])
+                                VALUES
+                                       (@Login, 
+		                               @FirstName,
+		                               @MiddleName,
+		                               @LastName,
+		                               @BirthDate)
+                                ";
+                        command.Parameters.Add(new SqlParameter("Login", newRecord.Login));
+                        command.Parameters.Add(new SqlParameter("FirstName", newRecord.FirstName));
+                        command.Parameters.Add(new SqlParameter("MiddleName", newRecord.MiddleName));
+                        command.Parameters.Add(new SqlParameter("LastName", newRecord.LastName));
+                        command.Parameters.Add(new SqlParameter("BirthDate", newRecord.BirthDate.ToString("s")));
+                        command.ExecuteNonQuery();
+
+                        command.Parameters.Clear();
+                        command.CommandText = "SELECT Id FROM Persons WHERE Login = @Login";
+                        command.Parameters.Add(new SqlParameter("Login", newRecord.Login));
+                        var personId = command.ExecuteScalar();
+
+                        command.Parameters.Clear();
+                        command.CommandText = @"
+                                INSERT INTO [dbo].[Addresses]
+                                       ([Street]
+                                       ,[CityId]
+                                       ,[HauseNr]
+                                       ,[ApartmentsNr]
+                                       ,[PersonId])
+                                VALUES
+                                       (@Street,
+                                       @CityId,                                            
+                                       @HouseNr,
+                                       @ApartmentsNr,
+                                       @PersonId)
+                                ";
+                        command.Parameters.Add(new SqlParameter("Street", newRecord.Street));
+                        command.Parameters.Add(new SqlParameter("CityId", newRecord.CityId));
+                        command.Parameters.Add(new SqlParameter("HouseNr", newRecord.HouseNr));
+                        command.Parameters.Add(new SqlParameter("ApartmentsNr", newRecord.ApartmentsNr));
+                        command.Parameters.Add(new SqlParameter("PersonId", personId));
+                        command.ExecuteNonQuery();
+
+                        transaction.Commit();
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    transaction.Rollback();
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+
+            UpdateDataView();
+
+            ClearInputs();
+        }
+
+        private void ClearInputs()
+        {
+            tbLogin.Clear();
+            tbFirstName.Clear();
+            tbMiddleName.Clear();
+            tbLastName.Clear();
+            tbStreet.Clear();
+            tbHouseNr.Clear();
+            tbApartmentsNr.Clear();
+        }
+
+        private void bnClear_Click(object sender, EventArgs e)
+        {
+            ClearInputs();
+        }
+    }
+
+    public class InputRecord
+    {
+        public string Login { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public long CountryId { get; set; }
+        public long CityId { get; set; }
+        public string Street { get; set; }
+        public string HouseNr { get; set; }
+        public string ApartmentsNr { get; set; }
+
+        public bool IsValid()
+        {
+            if (string.IsNullOrEmpty(Login))
+                return false;
+
+            if (string.IsNullOrEmpty(Street))
+                return false;
+
+            if (string.IsNullOrEmpty(HouseNr))
+                return false;
+
+            if (string.IsNullOrEmpty(ApartmentsNr))
+                return false;
+
+            return true;
         }
     }
 }

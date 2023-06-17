256b77adaa2b695d5c4ec47c931e7e25f7343065
Vasily Himchenko <White_Scorpion@inbox.lv>
2023-06-17 21:58:32 +0300

    UDP Client

---
+++ NetworkExperiments/FMain.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace NetworkExperiments
+{
+    partial class FMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lbResultsUDP = new ListBox();
+            bnListenUDP = new Button();
+            tbMessageUDP = new TextBox();
+            bnSendUDP = new Button();
+            SuspendLayout();
+            // 
+            // lbResultsUDP
+            // 
+            lbResultsUDP.FormattingEnabled = true;
+            lbResultsUDP.ItemHeight = 25;
+            lbResultsUDP.Location = new Point(12, 76);
+            lbResultsUDP.Name = "lbResultsUDP";
+            lbResultsUDP.Size = new Size(519, 454);
+            lbResultsUDP.TabIndex = 0;
+            // 
+            // bnListenUDP
+            // 
+            bnListenUDP.Location = new Point(12, 12);
+            bnListenUDP.Name = "bnListenUDP";
+            bnListenUDP.Size = new Size(134, 50);
+            bnListenUDP.TabIndex = 1;
+            bnListenUDP.Text = "Listen";
+            bnListenUDP.UseVisualStyleBackColor = true;
+            bnListenUDP.Click += bnListenUDP_Click;
+            // 
+            // tbMessageUDP
+            // 
+            tbMessageUDP.Location = new Point(12, 540);
+            tbMessageUDP.Name = "tbMessageUDP";
+            tbMessageUDP.Size = new Size(380, 31);
+            tbMessageUDP.TabIndex = 2;
+            // 
+            // bnSendUDP
+            // 
+            bnSendUDP.Location = new Point(398, 536);
+            bnSendUDP.Name = "bnSendUDP";
+            bnSendUDP.Size = new Size(133, 38);
+            bnSendUDP.TabIndex = 3;
+            bnSendUDP.Text = "Send";
+            bnSendUDP.UseVisualStyleBackColor = true;
+            bnSendUDP.Click += bnSendUDP_Click;
+            // 
+            // FMain
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(927, 762);
+            Controls.Add(bnSendUDP);
+            Controls.Add(tbMessageUDP);
+            Controls.Add(bnListenUDP);
+            Controls.Add(lbResultsUDP);
+            Name = "FMain";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox lbResultsUDP;
+        private Button bnListenUDP;
+        private TextBox tbMessageUDP;
+        private Button bnSendUDP;
+    }
+}
---
+++ NetworkExperiments/FMain.cs
@@ -0,0 +1,77 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace NetworkExperiments
+{
+    public partial class FMain : Form
+    {
+        private const int listeningPort = 43000;
+        private const int clientPort = 43001;
+        private readonly IPEndPoint _ipEndPoint;
+
+        private UdpClient _serverUdp;
+        private UdpClient _clientUdp;
+
+
+        public FMain()
+        {
+            InitializeComponent();
+
+            // 127.0.0.1
+            var ipAddressListeners = IPAddress.Any;
+            this._ipEndPoint = new IPEndPoint(ipAddressListeners, listeningPort);
+            this._serverUdp = new UdpClient(this._ipEndPoint);
+
+            var ipAddressClients = IPAddress.Parse("127.0.0.1");
+            var localEndpoint = new IPEndPoint(ipAddressClients, clientPort);
+            this._clientUdp = new UdpClient(localEndpoint);
+        }
+
+        private void bnListenUDP_Click(object sender, EventArgs e)
+        {
+            this._serverUdp.BeginReceive(new AsyncCallback(ServerReceiveCallback), this._serverUdp);
+        }
+
+        private void ServerReceiveCallback(IAsyncResult ar)
+        {
+            var localEndPoint = new IPEndPoint(IPAddress.Any, listeningPort);
+
+            byte[] buffer = _serverUdp.EndReceive(ar, ref localEndPoint);
+            var message = Encoding.UTF8.GetString(buffer);
+            UpdateCrossThread(lbResultsUDP, $"(Server) Received packet ({localEndPoint}): {message}");
+
+            if (!message.Equals("STOP"))
+                _serverUdp.BeginReceive(new AsyncCallback(ServerReceiveCallback), _serverUdp);
+        }
+
+        private void UpdateCrossThread(ListBox listBox, string message)
+        {
+            if (listBox.InvokeRequired)
+                listBox.Invoke(new Action(() => listBox.Items.Add(message)));
+            else
+                listBox.Items.Add(message);
+        }
+
+        private void bnSendUDP_Click(object sender, EventArgs e)
+        {
+            var messageToSend = tbMessageUDP.Text;
+            if (!string.IsNullOrEmpty(messageToSend))
+            {
+                var sentToEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), listeningPort);
+                var bytes = Encoding.UTF8.GetBytes(messageToSend);
+
+                this._clientUdp.BeginSend(bytes, bytes.Length, sentToEndPoint, new AsyncCallback(SendCallback),
+                    this._clientUdp);
+
+                tbMessageUDP.Clear();
+            }
+        }
+
+        private void SendCallback(IAsyncResult ar)
+        {
+            UdpClient u = (UdpClient)ar.AsyncState;
+            UpdateCrossThread(lbResultsUDP, $"Number of bytes sent: {u.EndSend(ar)}");
+        }
+    }
+}
